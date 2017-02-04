bab41b447db3bdbaec09fda2764cf8dce4be6fe1
dizpers <dizpers@gmail.com>
2017-02-04 14:31:08 +0700

    Modify html body

--- proxy.py
+++ proxy.py
@@ -1,11 +1,39 @@
-import io
-
 import asyncio
-
+import io
+import re
 from urllib.parse import urljoin
 
 import aiohttp
 from aiohttp import web
+
+
+def modify(s):
+    try:
+        body_start_index = s.index('<body')
+    except ValueError:
+        body_start_index = 0
+    try:
+        body_end_index = s.index('</body>')
+    except ValueError:
+        body_end_index = len(s) - 1
+    pre_body = s[:body_start_index]
+    body = s[body_start_index:body_end_index]
+    post_body = s[body_end_index:]
+    split_parts = re.split(r'[>\s](\w{6})(?!([^<]+)?>)(?=[^\w])', body, flags=re.UNICODE)
+    parts = []
+    need_append = True
+    forbidden_tags = ['<{0}{1}'.format(close, tag) for tag in ('script', 'style') for close in ('', '/')]
+    for part in split_parts:
+        if part is None:
+            continue
+        tags = {tag: part.rfind(tag) for tag in forbidden_tags}
+        if not all([index == -1 for index in tags.values()]):
+            need_append = '/' in max(tags, key=lambda item: tags[item])
+        if need_append and len(part) == 6:
+            part += '™'
+        parts.append(part)
+    body = ' '.join(parts)
+    return pre_body + body + post_body
 
 
 async def proxy_handler(request):
@@ -29,11 +57,19 @@ async def proxy_handler(request):
 
         await proxy_response.prepare(request)
 
-        # TODO: transform content here
+        need_to_modify = response.content_type == 'text/html'
+
         while True:
-            chunk = await response.content.read(io.DEFAULT_BUFFER_SIZE)
+            if need_to_modify:
+                chunk = await response.content.read()
+            else:
+                chunk = await response.content.read(io.DEFAULT_BUFFER_SIZE)
             if not chunk:
                 break
+            if need_to_modify:
+                chunk_str = chunk.decode(response.charset)
+                chunk_str = modify(chunk_str)
+                chunk = bytes(chunk_str, response.charset)
             proxy_response.write(chunk)
             await proxy_response.drain()
 
